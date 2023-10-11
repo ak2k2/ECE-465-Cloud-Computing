c3bd36aaa1af5e714ab0f0c09c800f184cc69185
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-11 00:05:15 -0400

    server client

--- assignment-2/client.py
+++ assignment-2/client.py
@@ -1,5 +1,38 @@
 import asyncio
-from asyncio import StreamReader, StreamWriter
+import concurrent.futures
+
+
+CHUNK_SIZE = 1024 * 4  # This can be adjusted based on your needs.
+
+
+def write_chunk(writer, chunk):
+    writer.write(chunk)
+
+
+async def send_in_chunks(writer, data):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        tasks = []
+        for i in range(0, len(data), CHUNK_SIZE):
+            chunk = data[i : i + CHUNK_SIZE]
+            task = await asyncio.to_thread(executor.submit, write_chunk, writer, chunk)
+            tasks.append(task)
+
+        # Wait for all threads to complete
+        for task in tasks:
+            task.result()  # This raises any exceptions encountered.
+
+        # After all the writes, drain the writer once to ensure all data has been sent.
+        await writer.drain()
+
+
+async def read_exactly(reader, num_bytes):
+    received_data = bytearray()
+    while len(received_data) < num_bytes:
+        chunk = await reader.read(min(num_bytes - len(received_data), CHUNK_SIZE))
+        if not chunk:
+            break
+        received_data.extend(chunk)
+    return bytes(received_data)
 
 
 async def send_request(command: str, filename: str):
@@ -7,12 +40,11 @@ async def send_request(command: str, filename: str):
 
     if command == "DOWNLOAD":
         writer.write(f"DOWNLOAD {filename}\n".encode())
-        response = await reader.readline()
-        if response == b"ERROR File not found\n":
+        data_size = int(await reader.readline())
+        content = await read_exactly(reader, data_size)
+        if content == b"ERROR File not found":
             print("File not found on the server.")
         else:
-            data = await reader.readuntil(EOF)
-            content = data[: -1 * len(EOF)]
             with open(f"client-files/{filename}", "wb") as f:
                 f.write(content)
             print("File downloaded successfully.")
@@ -21,23 +53,33 @@ async def send_request(command: str, filename: str):
         with open(f"client-files/{filename}", "rb") as f:
             content = f.read()
         writer.write(f"UPLOAD {filename}\n".encode())
-        # multithread here
-        writer.write(content)
-        writer.write(EOF)
+        writer.write(f"{len(content)}\n".encode())
+        await send_in_chunks(writer, content)
         response = await reader.readline()
         print(response.decode().strip())
 
     elif command == "PROCESS":
         writer.write(f"PROCESS {filename}\n".encode())
-        response = await reader.readline()
-        if response == b"ERROR File not found\n":
+        data_size = int(await reader.readline())
+        content = await read_exactly(reader, data_size)
+        if content == b"ERROR File not found":
             print("File not found on the server.")
         else:
-            data = await reader.readuntil(b"END\n")
-            content = data[:-4]
             with open(f"client-files/processed_{filename}", "wb") as f:
                 f.write(content)
             print("Processed image downloaded successfully.")
+
+    elif command == "HEQ":
+        with open(f"client-files/{filename}", "rb") as f:
+            content = f.read()
+        writer.write(f"HEQ {filename}\n".encode())
+        writer.write(f"{len(content)}\n".encode())
+        await send_in_chunks(writer, content)
+        data_size = int(await reader.readline())
+        content = await read_exactly(reader, data_size)
+        with open(f"client-files/processed_{filename}", "wb") as f:
+            f.write(content)
+        print("Processed image downloaded successfully.")
 
     elif command == "EXIT":
         writer.write(b"EXIT\n")
@@ -49,9 +91,9 @@ async def send_request(command: str, filename: str):
 
 
 if __name__ == "__main__":
-    command = input("Enter command (DOWNLOAD/UPLOAD/PROCESS/EXIT): ")
+    command = input("Enter command (DOWNLOAD/UPLOAD/PROCESS/HEQ/EXIT): ")
     filename = ""
-    if command in ["DOWNLOAD", "UPLOAD"]:
+    if command in ["DOWNLOAD", "UPLOAD", "HEQ"]:
         filename = input("Enter filename: ")
 
     asyncio.run(send_request(command, filename))
--- assignment-2/server.py
+++ assignment-2/server.py
@@ -7,8 +7,24 @@ import cv2
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 SERVER_DIRECTORY = "server-files"
+CHUNK_SIZE = 8192  # 8KB
+
+
+async def send_in_chunks(writer, data):
+    for i in range(0, len(data), CHUNK_SIZE):
+        writer.write(data[i : i + CHUNK_SIZE])
+        await writer.drain()
+
+
+async def read_exactly(reader, num_bytes):
+    received_data = bytearray()
+    while len(received_data) < num_bytes:
+        chunk = await reader.read(min(num_bytes - len(received_data), CHUNK_SIZE))
+        if not chunk:
+            break
+        received_data.extend(chunk)
+    return bytes(received_data)
 
 
 async def handle_client(reader: StreamReader, writer: StreamWriter):
@@ -32,29 +48,20 @@ async def handle_client(reader: StreamReader, writer: StreamWriter):
             else:
                 with open(filepath, "rb") as f:
                     content = f.read()
-                writer.write(b"OK\n")
-                writer.write(content)
-                await writer.drain()
-                writer.write(b"END\n")
-                await writer.drain()
+                writer.write(f"{len(content)}\n".encode())
+                await send_in_chunks(writer, content)
                 logger.info(f"Sent file {filename} to {client_addr}")
 
         elif command == "UPLOAD":
             filename = args[0]
             filepath = os.path.join(SERVER_DIRECTORY, filename)
-            try:
-                # set a timeout for readuntil
-                data = await asyncio.wait_for(reader.readuntil(b"END\n"), timeout=10.0)
-                data = data[:-4]
-                with open(filepath, "wb") as f:
-                    f.write(data)
-                writer.write(b"OK File saved\n")
-                await writer.drain()
-                logger.info(f"Received and saved file {filename} from {client_addr}")
-            except asyncio.TimeoutError:
-                writer.write(b"ERROR Timeout waiting for file data\n")
-                await writer.drain()
-                logger.error(f"Timeout error waiting for file data from {client_addr}")
+            data_size = int(await reader.readline())
+            data = await read_exactly(reader, data_size)
+            with open(filepath, "wb") as f:
+                f.write(data)
+            writer.write(b"OK File saved\n")
+            await writer.drain()
+            logger.info(f"Received and saved file {filename} from {client_addr}")
 
         elif command == "PROCESS":
             filename = args[0]
@@ -70,20 +77,42 @@ async def handle_client(reader: StreamReader, writer: StreamWriter):
                 # Apply histogram equalization
                 equ = cv2.equalizeHist(img)
 
-                # Save the equalized image (optional step if you want to save the processed image on the server)
-                # cv2.imwrite(f"{filepath}_equalized.png", equ)
-
                 # Convert the equalized image to bytes
                 _, img_encoded = cv2.imencode(".png", equ)
                 img_bytes = img_encoded.tobytes()
 
-                # Send the processed image back to the client
-                writer.write(b"OK\n")
-                writer.write(img_bytes)
-                await writer.drain()
-                writer.write(b"END\n")
-                await writer.drain()
+                writer.write(f"{len(img_bytes)}\n".encode())
+                await send_in_chunks(writer, img_bytes)
                 logger.info(f"Sent processed file {filename} to {client_addr}")
+
+        elif command == "HEQ":
+            filename = args[0]
+            data_size = int(await reader.readline())
+            data = await read_exactly(reader, data_size)
+
+            # Use a temporary path to save the uploaded image for processing
+            temp_filepath = os.path.join(SERVER_DIRECTORY, f"temp_{filename}")
+            with open(temp_filepath, "wb") as f:
+                f.write(data)
+
+            # Read the image using OpenCV
+            img = cv2.imread(temp_filepath, cv2.IMREAD_GRAYSCALE)
+
+            # Apply histogram equalization
+            equ = cv2.equalizeHist(img)
+
+            # Convert the equalized image to bytes
+            _, img_encoded = cv2.imencode(".png", equ)
+            img_bytes = img_encoded.tobytes()
+
+            writer.write(f"{len(img_bytes)}\n".encode())
+            await send_in_chunks(writer, img_bytes)
+            logger.info(
+                f"Received, processed, and sent file {filename} from/to {client_addr}"
+            )
+
+            # Remove the temp file after processing
+            os.remove(temp_filepath)
 
         elif command == "EXIT":
             writer.write(b"OK Bye\n")

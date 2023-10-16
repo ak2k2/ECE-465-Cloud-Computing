9c3d8743ba75970c7e6a18043f2f440d1ef7d5cc
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 13:33:54 -0400

    Update ray-server.py

--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -6,7 +6,11 @@ import time
 def main():
     # Initialize Ray with default options.
     # It will start the relevant servers and listen on all interfaces.
-    ray.init(_node_ip_address="0.0.0.0:6379", _redis_password="5241590000000000")
+    ray.init(
+        _node_ip_address="0.0.0.0",
+        _redis_password="5241590000000000",
+        dashboard_host="0.0.0.0",
+    )
     # ray.init()
 
     print("Ray Server Started...")

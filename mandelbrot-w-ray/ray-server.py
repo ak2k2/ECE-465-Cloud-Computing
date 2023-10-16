c16a06a924413502d9d7f4e2a093d860dd6d708f
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 04:46:36 -0400

    start if not exist

--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -11,14 +11,25 @@ logging.basicConfig(
 )
 
 
-def main():
-    # Initialize Ray
+def start_ray():
     try:
-        # Try to start the Ray server
-        ray.init(address="0.0.0.0:6379")
-        logging.info("Ray server started successfully.")
-    except Exception as e:
-        logging.error("Could not start Ray server: {}".format(e))
+        # Specify the address of the existing cluster here
+        ray.init("ray://192.168.1.11:6379")
+        logging.info("Connected to the existing Ray cluster.")
+    except ConnectionError:
+        logging.warning(
+            "No existing Ray cluster found or connection failed. Trying to start a new cluster."
+        )
+        try:
+            # Try to start a new Ray cluster
+            ray.init(address="0.0.0.0:6379")
+            logging.info("New Ray cluster started successfully.")
+        except Exception as e:
+            logging.error("Could not start Ray server: {}".format(e))
+
+
+def main():
+    start_ray()
 
     print("Ray Server Started...")
     while True:

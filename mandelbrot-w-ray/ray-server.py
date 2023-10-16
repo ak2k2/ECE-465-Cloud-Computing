595144d5833c2e1cfbbdecc00a482a10eceea18d
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 04:54:35 -0400

    start on first

--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -4,9 +4,11 @@ import time
 
 
 def main():
-    # Initialize Ray. No parameters means it starts in local mode but listens on all interfaces.
-    ray.init(address="auto", _redis_password="5241590000000000")
+    # Initialize Ray with default options.
+    # It will start the relevant servers and listen on all interfaces.
+    ray.init(_redis_password="5241590000000000")
 
+    # Print the Ray dashboard URL, which is useful for monitoring the Ray cluster.
     print(f"Dashboard URL: {ray.get_dashboard_url()}")
 
     print("Ray Server Started...")

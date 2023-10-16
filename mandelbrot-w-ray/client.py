4bda90db1f0c9d41366d0c52aa83bef1bfe640e2
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 05:06:44 -0400

    defaults

--- mandelbrot-w-ray/client.py
+++ mandelbrot-w-ray/client.py
@@ -11,7 +11,7 @@ import io
 
 
 def connect_to_server():
-    ray.init(address="ray://192.168.1.11:6379", _redis_password="5241590000000000")
+    ray.init(address="auto")  # connect to the running Ray cluster
 
 
 def compile_video(frame_images: list, fps: int = 15) -> str:
--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -6,11 +6,12 @@ import time
 def main():
     # Initialize Ray with default options.
     # It will start the relevant servers and listen on all interfaces.
-    ray.init(
-        _node_ip_address="192.168.1.11",
-        _redis_password="5241590000000000",
-        dashboard_host="0.0.0.0",
-    )
+    # ray.init(
+    #     _node_ip_address="192.168.1.11",
+    #     _redis_password="5241590000000000",
+    #     dashboard_host="0.0.0.0",
+    # )
+    ray.init()
 
     print("Ray Server Started...")
     while True:

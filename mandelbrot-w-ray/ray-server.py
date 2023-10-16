4900efbb5d754adbcc5c0ee599d7825bd8806abf
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 04:55:04 -0400

    Update ray-server.py

--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -8,9 +8,6 @@ def main():
     # It will start the relevant servers and listen on all interfaces.
     ray.init(_redis_password="5241590000000000")
 
-    # Print the Ray dashboard URL, which is useful for monitoring the Ray cluster.
-    print(f"Dashboard URL: {ray.get_dashboard_url()}")
-
     print("Ray Server Started...")
     while True:
         print("♥")

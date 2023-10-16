748a99a8417b012c6ff188b0efebc763f0f586e6
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 04:35:03 -0400

    network trsuted

--- mandelbrot-w-ray/ray-server.py
+++ mandelbrot-w-ray/ray-server.py
@@ -5,11 +5,7 @@ import time
 
 def main():
     # Initialize Ray
-    ray.init(
-        address="0.0.0.0:6379",
-        _redis_password="5241590000000000",
-        _node_ip_address="0.0.0.0",
-    )
+    ray.init(_node_ip_address="0.0.0.0")
 
     print("Ray Server Started...")
     while True:

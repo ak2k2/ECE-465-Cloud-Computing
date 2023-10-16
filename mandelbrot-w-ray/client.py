eabd5dd3b42d2c9468934d6df5f50afeb31616f5
ak2k2 <armaan.kapoor@cooper.edu>
2023-10-16 13:41:01 -0400

    Update client.py

--- mandelbrot-w-ray/client.py
+++ mandelbrot-w-ray/client.py
@@ -11,9 +11,7 @@ from PIL import Image
 
 
 def connect_to_server():
-    ray.init(
-        address="ray://192.168.1.11:6379", _redis_password="5241590000000000"
-    )  # connect to the running Ray cluster
+    ray.init(_redis_password="5241590000000000")  # connect to the running Ray cluster
 
 
 def compile_video(frame_images: list, fps: int = 15) -> str:
